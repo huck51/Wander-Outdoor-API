1c87019cfa1f9f4448f5223bb54eed85524b8ab2
Huck <evanallen51@gmail.com>
2018-04-17 02:00:44 -0500

    Setting up mongoDB on the server

--- server.js
+++ server.js
@@ -5,8 +5,8 @@ const PORT = process.env.PORT || 5001;
 
 const server = express();
 
-// mongoose.Promise = global.Promise;
-// mongoose.connect('mongodb://localhost/wander', { useMongoClient: true });
+mongoose.Promise = global.Promise;
+mongoose.connect('mongodb://<dbuser>:<dbpassword>@ds247569.mlab.com:47569/heroku_l8cdgv7t');
 
 server.use(bodyParser.json());
 server.use(bodyParser.urlencoded({extended: true}));
