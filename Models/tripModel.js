2b97e672b00b32ebe5ae9741bd53e05b1696e7f0
Huck <evanallen51@gmail.com>
2018-04-18 01:33:43 -0500

    Fixed Trip model error

--- Models/tripModel.js
+++ Models/tripModel.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
-const Trip = new Schema({
+const TripSchema = new Schema({
   name: {
     type: String,
     required: true,
