b764f94e28fbc15a9440995d9f5e93c40856c348
Huck <evanallen51@gmail.com>
2018-04-17 21:10:28 -0500

    Imported Models to server. Initialized routes

--- server.js
+++ server.js
@@ -2,17 +2,33 @@ const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const PORT = process.env.PORT || 5001;
+const Traveler = require('./Models/travelerModel');
+const Guide = require('./Models/guideModel');
+const Company = require('./Models/companyModel');
+const Trip = require('./Models/tripModel');
 
 const server = express();
 
 mongoose.Promise = global.Promise;
-mongoose.connect('mongodb://heroku_l8cdgv7t@ds247569.mlab.com:47569/heroku_l8cdgv7t');
+mongoose.connect(process.env.MONGODB_URI);
 
 server.use(bodyParser.json());
 server.use(bodyParser.urlencoded({extended: true}));
 
 server.get('/', (req, res) => {
   res.send('SERVES UP DOOD');
+});
+
+server.post('/signup/traveler', (req, res) => {
+  const body = req.body;
+});
+
+server.post('/signup/guide', (req, res) => {
+  const body = req.body;
+});
+
+server.post('/signup/guiding-company', (req, res) => {
+  const body = req.body;
 });
 
 server.listen(PORT, () => {
