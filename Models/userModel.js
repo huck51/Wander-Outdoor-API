755fbc62a33779262e9d6d3ef58664f0fef6bb67
Huck <evanallen51@gmail.com>
2018-05-24 13:50:58 -0500

    Added new User model

---
+++ Models/userModel.js
@@ -0,0 +1,53 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const UserSchema = new Schema({
+  firstName: {
+    type: String,
+  },
+  lastName: {
+    type: String,
+  },
+  companyName: {
+    type: String,
+  },
+  companyCode: {
+    type: String,
+  },
+  DOB: {
+    type: Date,
+  },
+  email: {
+    type: String,
+    required: true,
+  },
+  phone: {
+    type: String,
+  },
+  username: {
+    type: String,
+    unique: true,
+  },
+  picture: {
+    data: Buffer,
+    contentType: String,
+  },
+  stats: {},
+  tripsQualified: {
+    type: [String],
+  },
+  tripsCompleted: {
+    type: [String],
+  },
+  certs: {
+    type: [String],
+  },
+  activities: {
+    type: [String],
+  },
+  rating: {
+    type: Number,
+  },
+});
+
+module.exports = mongoose.model('User', UserSchema);
