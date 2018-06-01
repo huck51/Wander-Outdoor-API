18373f94ab2579f203a6b55950da45a0fe89a015
Huck <evanallen51@gmail.com>
2018-06-01 18:11:09 -0500

    Added logs to the update-profile route

--- server.js
+++ server.js
@@ -251,6 +251,8 @@ server.post('/contact-message', (req, res) => {
 server.post('/update-profile', (req, res) => {
   console.log('updateprofile');
   const { updateObject } = req.body;
+  console.log(updateObject):
+  conosle.log(req.body);
   User.findOneAndUpdate({ id: updateObject.id }, updateObject, (err, updatedUser) => {
     if (err) {
       res.status(422).send(err);
