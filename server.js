838031aa46a0111afcfd0644b38ab08c4c786b77
Huck <evanallen51@gmail.com>
2018-10-10 16:35:59 -0500

    Reconfigured Cors options

--- server.js
+++ server.js
@@ -47,12 +47,12 @@ mongoose.connect(process.env.MONGODB_URI, mongOptions)
 server.use(bodyParser.json());
 server.use(bodyParser.urlencoded({extended: true}));
 
-// server.use((req, res, next) => {
-//     res.setHeader('Access-Control-Allow-Origin', '*');
-//     res.setHeader('Access-Control-Allow-Methods', 'POST, GET, OPTIONS, DELETE');
-//
-//     next();
-// });
+server.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+
+    next();
+});
 
 server.use(cors());
 
