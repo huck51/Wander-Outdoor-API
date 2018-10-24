b35b0e925b85e0dd62945f4d32eff8c2f992e1e2
Huck <evanallen51@gmail.com>
2018-10-23 20:14:00 -0500

    Added new field to those being returned by /guides

--- server.js
+++ server.js
@@ -280,7 +280,7 @@ server.post('/find-user', (req, res) => {
 });
 
 server.get('/guides', (req, res) => {
-  User.find({ roleGroup: 'guide' }, { firstName: 1, lastName:1, companyName: 1, companyEmail: 1, city: 1, stateName: 1, roleGroup: 1, picture: 1, id: 1, rating: 1, _id: 0 }, (err, allGuides) => {
+  User.find({ roleGroup: 'guide' }, { firstName: 1, lastName:1, name: 1, companyName: 1, companyEmail: 1, city: 1, stateName: 1, roleGroup: 1, picture: 1, id: 1, rating: 1, _id: 0 }, (err, allGuides) => {
     if (err) {
       res.status(422);
       res.json({ stack: err.stack, message: err.message });
