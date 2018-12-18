2997fded73423639f015faa03b2c9d101196d258
Huck <evanallen51@gmail.com>
2018-12-17 20:49:53 -0600

    Added new setup route

--- server.js
+++ server.js
@@ -458,6 +458,19 @@ server.post('/unlink-guide-from-trip', (req, res) => {
   });
 });
 
+server.get('/setup-profile/:id', (req, res) => {
+  const { id } = req.params;
+  User.findOne({ id }, { firstName: 1, lastName: 1, profileNum: 1, roleGroup: 1 }(err, foundUser) => {
+    if (err) {
+      console.log(err);
+      return res.status(503).send(err);
+    }
+    if (foundUser) {
+      return res.status(200).json(foundUser);
+    }
+  });
+});
+
 server.post('/signup-newuser', (req, res) => {
   const { id, email, sub } = req.body;
   const profileNum = cryptoRandomString(25);
